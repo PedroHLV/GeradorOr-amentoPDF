59bc79f181b86590e6f3094db8623e572ac8f665
Pedro Henrique Lopes Vieira <pedro.lopes@tvmorena.com.br>
2024-01-29 10:03:07 -0400

    firstCommit

---
+++ app.py
@@ -0,0 +1,4 @@
+descricao_projeto = input("Digite a descrição do projeto: ")
+horas_previstas = input("Digite a quantidade de horas previstas: ")
+valor_hora = input("Digite o valor da hora trabalhada: ")
+prazo = input("Digite o prazo estimado: ")
